acaa6fcacfbac5877c2815e6af34e8dcbfebe039
romanov05 <yuriy.romanov30@gmail.com>
2026-05-31 16:38:55 +0300

    Change URL

--- book-house-service-modules/book-house-service-urls.js
+++ book-house-service-modules/book-house-service-urls.js
@@ -4,7 +4,7 @@ class BookHouseServiceUrls {
     }
 
     getServices(title = '') {
-        const url = `${this.baseUrl}/services`;
+        const url = `${this.baseUrl}/book-house-service`;
 
         if (title && title.trim() !== "") {
             return `${url}?title=${title}`;
@@ -14,19 +14,19 @@ class BookHouseServiceUrls {
     }
 
     getServiceById(id) {
-        return `${this.baseUrl}/services/${id}`;
+        return `${this.baseUrl}/book-house-service/${id}`;
     }
 
     createService() {
-        return `${this.baseUrl}/services`;
+        return `${this.baseUrl}/book-house-service`;
     }
 
     removeServiceById(id) {
-        return `${this.baseUrl}/services/${id}`;
+        return `${this.baseUrl}/book-house-service/${id}`;
     }
 
     updateServiceById(id) {
-        return `${this.baseUrl}/services/${id}`;
+        return `${this.baseUrl}/book-house-service/${id}`;
     }
 }
 
